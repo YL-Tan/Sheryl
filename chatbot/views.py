01caf42fffe1709d7b6b02c16c03569049dd4d14
YL-Tan <ytan@seattleu.edu>
2023-06-28 22:29:17 -0700

    Set up user authentication

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
 import json
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 def ask_openai(message):
     with open('api.json', 'r') as file_to_read:
@@ -27,3 +29,45 @@ def chatbot(request):
         response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = "Invalid username or password"
+            return render(request, "login.html", {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+    
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password2)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = "Failed to create your account"
+                return render(request, "register.html", {'error_message': error_message})
+        else:
+            error_message = 'Passwords did not match'
+            return render(request, 'register.html', {'error_message': error_message})
+        
+    return render(request, "register.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+    
