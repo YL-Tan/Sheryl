2fa288886d1d6828ba6d2e9bfed55030d700ba4f
YL-Tan <ytan@seattleu.edu>
2023-07-03 09:57:31 -0700

    Finished database and setup chat completion

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,31 +4,53 @@ import openai
 import json
 from django.contrib import auth
 from django.contrib.auth.models import User
+from .models import Chat
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+from django.utils import timezone
 
 def ask_openai(message):
     with open('api.json', 'r') as file_to_read:
         json_data = json.load(file_to_read)
         openai.api_key = json_data["openai_api_key"]
         
-    response = openai.Completion.create(
-        model = "text-curie-001",
-        prompt = message,
-        max_tokens = 150,
-        n = 1,
-        stop = None,
-        temperature = 0.7,
+    # response = openai.Completion.create(
+    #     model = "text-curie-001",
+    #     prompt = message,
+    #     max_tokens = 150,
+    #     n = 1,
+    #     stop = None,
+    #     temperature = 0.7,
+    # )
+    
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "system", "content": "You are an helpful assistant."},
+            {"role": "user", "content": message},
+        ]     
     )
-
-    answer = response.choices[0].text.strip()
-    return answer
+    answer = response.choices[0].message.content.strip()
+    print("\n" + answer + "\n")
+    formatter = HtmlFormatter()
+    code_html = highlight(answer, PythonLexer(), formatter)
+    return code_html
     
 # Create your views here.
 def chatbot(request):
+    chats = Chat.objects.filter(user=request.user)  # filter to get the current logged in user
+    
     if request.method == 'POST':
         message = request.POST.get('message')
         response = ask_openai(message)
+    
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
         return JsonResponse({'message': message, 'response': response})
-    return render(request, 'chatbot.html')
+    return render(request, 'chatbot.html', {'chats': chats})
 
 def login(request):
     if request.method == 'POST':
