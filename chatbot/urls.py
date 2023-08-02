c034cc6f277622f5a1ae3b62014116e9000f2fc6
YL-Tan <ytan@seattleu.edu>
2023-08-02 13:10:55 -0700

    Add documentation on urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,3 +1,7 @@
+""" This file is a configuration file for defining URL patterns and 
+associating them with corresponding view functions in the Django 
+application
+"""
 from django.urls import path
 from . import views
 
